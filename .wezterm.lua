e8e01fc1aa50adf83d7ec70c90b0a3936dd7f980
Tien-Shuo Chang <yorkygrizzly@gmail.com>
2025-04-23 10:45:29 -0700

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,8 +6,11 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 config.color_scheme = "Dark+"
-wezterm.font("JetBrainsMono Nerd Font Propo", { weight = "Regular", stretch = "Normal", style = "Normal" })
-wezterm.font_size = 13.0
+
+-- wezterm.font("JetBrainsMono Nerd Font Propo", { weight = "Regular", stretch = "Normal", style = "Normal" })
+config.font = wezterm.font("Maple Mono NF CN", { weight = "Regular", stretch = "Normal", style = "Normal" })
+
+config.font_size = 12.0
 config.window_background_opacity = 0.75
 config.hide_tab_bar_if_only_one_tab = true
 config.keys = {
@@ -17,6 +20,11 @@ config.keys = {
 		action = wezterm.action.ToggleFullScreen,
 	},
 }
+
+-- This setting controls the maximum frame rate used when rendering easing effects for blinking cursors, blinking text and visual bell.
+config.animation_fps = 60
+-- Limits the maximum number of frames per second that wezterm will attempt to draw.
+config.max_fps = 60
 
 -- and finally, return the configuration to wezterm
 return config
