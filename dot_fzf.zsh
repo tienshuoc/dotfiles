56421a03c338fe44baea3dc4a1ad1e5407e822f4
Tien-Shuo Chang <tienshuo.chang@sambanovasystems.com>
2025-09-02 04:00:11 +0000

    bazel + fzf

--- dot_fzf.zsh
+++ dot_fzf.zsh
@@ -8,4 +8,81 @@ fi
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
+# Bazel + FZF Integration
+_fzf_complete_bazel_test() {
+    _fzf_complete '-m' "$@" < <(command bazel query \
+        "kind('(test|test_suite) rule', //...)" 2>/dev/null)
+}
+
+_fzf_complete_bazel_build() {
+    _fzf_complete '-m' "$@" < <(command bazel query \
+        "kind('rule', //...) except kind('(test|test_suite) rule', //...)" 2>/dev/null)
+}
+
+_fzf_complete_bazel_run() {
+    _fzf_complete '-m' "$@" < <(command bazel query \
+        "kind('.*_binary rule', //...)" 2>/dev/null)
+}
+
+_fzf_complete_bazel() {
+    local tokens
+    tokens=($(printf "%s" "$LBUFFER" | xargs -n 1))
+
+    if [ ${#tokens[@]} -ge 3 ]; then
+        case "${tokens[2]}" in
+        test)
+            _fzf_complete_bazel_test "$@"
+            ;;
+        build)
+            _fzf_complete_bazel_build "$@"
+            ;;
+        run)
+            _fzf_complete_bazel_run "$@"
+            ;;
+        *)
+            # Original completion for other commands
+            _fzf_complete '-m' "$@" < <(command bazel query --keep_going \
+                --noshow_progress \
+                "kind('(binary rule)|(generated file)', deps(//...))" 2>/dev/null)
+            ;;
+        esac
+    else
+        # Original completion when no command specified
+        _fzf_complete '-m' "$@" < <(command bazel query --keep_going \
+            --noshow_progress \
+            "kind('(binary rule)|(generated file)', deps(//...))" 2>/dev/null)
+    fi
+}
+
+_fzf_complete_git_add() {
+    _fzf_complete "-m --preview 'git diff --color=always {}'" "$@" < <(
+    command git ls-files --modified --others --exclude-standard
+)
+}
+
+_fzf_complete_git_restore() {
+    _fzf_complete "-m --preview 'git diff --color=always {}'" "$@" < <(
+    command git ls-files --modified
+)
+}
+
+_fzf_complete_git() {
+    local tokens
+    tokens=($(printf "%s" "$LBUFFER" | xargs -n 1))
+
+    if [ ${#tokens[@]} -ge 3 ]; then
+        case "${tokens[2]}" in
+        add)
+            _fzf_complete_git_add "$@"
+            ;;
+        restore)
+            _fzf_complete_git_restore "$@"
+            ;;
+        esac
+    else
+        # Original completion when no command specified
+    fi
+    _fzf_complete "$@"
+}
+
 source <(fzf --zsh)
